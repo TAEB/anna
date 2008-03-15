339ecf9e1dfab3801bd93cad9f0b90262e730a92
alexbobp <alexbobp@gmail.com>
2008-03-15 16:53:12 -0400

    wrote server() in KralnServer and made empty constructor that throws the exceptions it should

--- src/annahack/dglconnection/KralnServer.java
+++ src/annahack/dglconnection/KralnServer.java
@@ -1,6 +1,16 @@
 package annahack.dglconnection;
 
+import org.apache.commons.net.telnet.InvalidTelnetOptionException;
+import java.io.IOException;
+import java.net.SocketException;
+
 public class KralnServer extends GeneralDGLConnector implements DGLServer
 {
-
+	public KralnServer() throws InvalidTelnetOptionException, SocketException, IOException
+	{
+	}
+	public String server()
+	{
+		return "nethack.kraln.com";
+	}
 }
