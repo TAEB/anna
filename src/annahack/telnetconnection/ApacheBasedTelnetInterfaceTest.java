d09e1c73bcb7e5b83bd259715db9c0d33de51817
alexbobp <alexbobp@gmail.com>
2008-03-15 17:52:47 -0400

    Copied telnet init code into the tester

--- src/annahack/telnetconnection/ApacheBasedTelnetInterfaceTest.java
+++ src/annahack/telnetconnection/ApacheBasedTelnetInterfaceTest.java
@@ -1,5 +1,8 @@
 package annahack.telnetconnection;
 
+import org.apache.commons.net.telnet.*;
+import java.io.IOException;
+
 public class ApacheBasedTelnetInterfaceTest
 {
 
@@ -7,9 +10,22 @@ public class ApacheBasedTelnetInterfaceTest
 	 * @param args
 	 */
 	public static void main(String[] args)
+		throws InvalidTelnetOptionException, IOException
 	{
-		// TODO Auto-generated method stub
+		TelnetClient tc;
+		tc = new TelnetClient();
 
+        TerminalTypeOptionHandler ttopt = new TerminalTypeOptionHandler("VT100", false, false, true, false);
+        EchoOptionHandler echoopt = new EchoOptionHandler(true, false, true, false);
+        SuppressGAOptionHandler gaopt = new SuppressGAOptionHandler(true, true, true, true);
+
+        tc.addOptionHandler(ttopt);
+        tc.addOptionHandler(echoopt);
+        tc.addOptionHandler(gaopt);
+        
+		tc.connect("nethack.kraln.com", 23);
+		
+		TelnetInterface connection=new ApacheBasedTelnetInterface(tc);
 	}
 
 }
