7cab1b50fff32e72701db855670711bb3e431dbc
alexbobp <alexbobp@gmail.com>
2008-03-15 17:34:30 -0400

    added tester for telnet interface ApacheBasedTelnetInterfaceTest

---
+++ src/annahack/telnetconnection/ApacheBasedTelnetInterfaceTest.java
@@ -0,0 +1,15 @@
+package annahack.telnetconnection;
+
+public class ApacheBasedTelnetInterfaceTest
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
