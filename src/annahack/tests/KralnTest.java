5911f32b03584c142d21d8cbaf895a57075d43c5
mattwelborn <mattwelborn@mail.utexas.edu>
2008-03-20 14:19:33 -0400

    Added tester for KralnServer

---
+++ src/annahack/tests/KralnTest.java
@@ -0,0 +1,30 @@
+package annahack.tests;
+
+import annahack.dglconnection.KralnServer;
+
+public class KralnTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Throwable //I'm a bad person
+	{
+		KralnServer server=new KralnServer();
+		if(server.login())
+		{
+			if(server.startGame())
+			{
+				System.out.println("Victory");
+			}
+			else
+			{
+				System.err.println("startGame failed");
+			}
+		}
+		else
+		{
+			System.err.println("login failed");
+		}
+		
+	}
+}
