b832466b119c74e33186234bbb50078009146c76
alexbobp <alexbobp@gmail.com>
2008-03-19 23:23:00 -0400

    Added UniqueNameGetter, with some of a constructor

---
+++ src/annahack/utils/UniqueNameGetter.java
@@ -0,0 +1,25 @@
+package annahack.utils;
+
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class UniqueNameGetter
+{
+	private String[] names;
+	public UniqueNameGetter() throws FileNotFoundException, IOException
+	{
+		{
+			//Read in names from file
+			BufferedReader fread=new BufferedReader(new FileReader("names"));
+			ArrayList<String> namestmp=new ArrayList<String>();
+			while (fread.ready())
+				namestmp.add(fread.readLine());
+			names=namestmp.toArray(new String[namestmp.size()]); 
+		}
+		
+		
+	}
+}
