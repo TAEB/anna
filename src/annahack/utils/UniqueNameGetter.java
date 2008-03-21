60824f259d37f2b741e79693ab3a89b7b779458d
alexbobp <alexbobp@gmail.com>
2008-03-21 13:46:48 -0400

    Moved ThreadedShuffler to private inner class of UniqueNameGetter

--- src/annahack/utils/UniqueNameGetter.java
+++ src/annahack/utils/UniqueNameGetter.java
@@ -1,6 +1,7 @@
 package annahack.utils;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
@@ -34,5 +35,76 @@ public class UniqueNameGetter
 			return "";
 		else
 			return new Integer(i).toString();
+	}
+	
+	private class ThreadedShuffler<T> implements Runnable
+	{
+		private T[] target;
+		private int shufflei;
+		private int geti;
+		private int shufflecount;
+		private Thread shuffler;
+		private Random rand;
+		
+		public ThreadedShuffler(T[] target)
+		{
+			this.target=target;
+			shufflei=0;
+			shufflecount=0;
+			shuffler=new Thread(this);
+			shuffler.start();
+			rand=new Random();
+		}
+		
+		private void makeThread()
+		{
+			shuffler=new Thread(this);
+			shuffler.start();
+		}
+		
+		private T tswap;
+		private int swapi;
+		public void run()
+		{
+			for (shufflei=0; shufflei<target.length-1; shufflei++)
+			{
+				swapi=shufflei+rand.nextInt(target.length-shufflei);
+				
+				tswap=target[shufflei];
+				target[shufflei]=target[swapi];
+				target[swapi]=tswap;
+			}
+		}
+		
+		public T getNext()
+		{
+			while (geti>=shufflei-1) //Block while waiting for an available item
+			{
+				try
+				{
+					Thread.sleep(10);
+				}catch(InterruptedException e)
+				{
+				}
+			}
+			
+			T ret=target[geti++];
+			
+			//Start another sort if we have expended this array
+			if (geti==target.length)
+			{
+				geti=0;
+				shufflei=0;
+				shufflecount++;
+				makeThread();
+			}
+			
+			return ret;
+		}
+		
+		public int getShuffleCount()
+		{
+			return shufflecount;
+		}
 	}
 }
