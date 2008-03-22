9986a2d8c12f451b77ec5cf41d2f787da13002f0
alexbobp <alexbobp@gmail.com>
2008-03-22 00:33:26 -0400

    Made separate getLoggedInInterface method in NetHackParserTest

--- src/annahack/tests/NetHackParserTest.java
+++ src/annahack/tests/NetHackParserTest.java
@@ -3,6 +3,7 @@ package annahack.tests;
 import org.apache.commons.net.telnet.*;
 import annahack.telnetconnection.*;
 import annahack.nethackparser.*;
+import annahack.dglconnection.*;
 
 import java.io.IOException;
 import java.io.BufferedReader;
@@ -11,25 +12,30 @@ import java.io.OutputStream;
 
 public class NetHackParserTest
 {
-	public static void main(String[] args)
-	throws InvalidTelnetOptionException, IOException, InterruptedException
+	public static void main(String[] args) throws Exception
 	{
-		TelnetClient tc;
-		tc = new TelnetClient();
-
-        TerminalTypeOptionHandler ttopt = new TerminalTypeOptionHandler("VT100", false, false, true, false);
-        EchoOptionHandler echoopt = new EchoOptionHandler(true, false, true, false);
-        SuppressGAOptionHandler gaopt = new SuppressGAOptionHandler(true, true, true, true);
-
-        tc.addOptionHandler(ttopt);
-        tc.addOptionHandler(echoopt);
-        tc.addOptionHandler(gaopt);
-        
-		tc.connect("nethack.kraln.com", 23);
-		OutputStream os=tc.getOutputStream();
+		NetHackParser nhp=new NetHackParser(getLoggedInInterface());
 		
-		TelnetInterface connection=new ApacheBasedTelnetInterface(tc);
 		
-		NetHackParser nhp=new NetHackParser(connection);
+	}
+	
+	/*
+	 * A method like this might be useful outside of testing,
+	 * but it would require modification to be more robust
+	 */
+	private static TelnetInterface getLoggedInInterface() throws Exception
+	{
+		DGLServer kraln=new KralnServer();
+		if (!kraln.login())
+		{
+			System.out.println("Login failed!");
+			return null;
+		}
+		if (!kraln.startGame())
+		{
+			System.out.println("Game start failed!");
+			return null;
+		}
+		return kraln.dumpInterface();
 	}
 }
