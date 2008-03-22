35c3488a85a20ffdb0bf4b75541a7c5aad12bce6
alexbobp <alexbobp@gmail.com>
2008-03-22 17:20:10 -0400

    Added some of the message parsing to NetHackParser

--- src/annahack/nethackparser/NetHackParser.java
+++ src/annahack/nethackparser/NetHackParser.java
@@ -2,11 +2,13 @@ package annahack.nethackparser;
 
 import annahack.telnetconnection.*;
 import java.util.Queue;
+import java.io.IOException;
 
 public class NetHackParser
 {
 	private TelnetInterface com;
 	private Queue<String> messageBuf;
+	private Queue<String> itemsBuf;
 	
 	public NetHackParser(TelnetInterface com)
 	{
@@ -14,4 +16,47 @@ public class NetHackParser
 		messageBuf=new java.util.LinkedList<String>();
 	}
 	
+	/**
+	 * @return
+	 * 0 if there were no messages,
+	 * 1 if there were messages
+	 * 2 if there were messages and space needs to be sent (--more--)
+	 * 3 if the last message is a prompt
+	 */
+	public byte checkMessages() throws IOException
+	{
+		String line;
+		line=new String(com.peekLine(0));
+		
+		if (line.equals("                        "))
+		{
+			//Nothing
+			return 0;
+		}else{
+			//Something
+			int search=line.indexOf("--More--");
+			if (search!=-1)
+			{
+				//Messages and --More--
+				String[] msgs=line.substring(0, search).split("  ");
+				for (int i=0; i<msgs.length; i++)
+				{
+					messageBuf.add(msgs[i]);
+				}
+			}else{
+				//No --More-- on first lines
+				search=line.indexOf("Things that are here");
+				if (search!=-1)
+				{
+					//Item list
+					
+				}else{
+					
+				}
+				
+			}
+		}
+		
+		return 0;
+	}
 }
