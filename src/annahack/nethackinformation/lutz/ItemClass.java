5122b0d6a5815dc644d61fb6578651c6c9d468a7
mattwelborn <mattwelborn@mail.utexas.edu>
2008-03-22 16:29:26 -0400

    Added to the ItemClass interface

--- src/annahack/nethackinformation/lutz/ItemClass.java
+++ src/annahack/nethackinformation/lutz/ItemClass.java
@@ -4,9 +4,12 @@ public interface ItemClass
 {
 	public byte mapCharacter();
 	
+	public boolean dipinable();
 	public boolean quaffable();
 	public boolean edible();
 	public boolean applyable();	//potion of oil can shove it
-	public boolean wearable();
-	public boolean putonable();
+	public boolean wearable(); //As in armor
+	public boolean putonable(); //As in jewelry
+	public boolean readable();
+	public boolean zappable();
 }
