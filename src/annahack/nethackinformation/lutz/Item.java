bd4fd1e62d8b2e4cbc48d931686a87eb77e4a121
alexbobp <alexbobp@gmail.com>
2008-03-22 15:39:44 -0400

    added some functions to Item class

--- src/annahack/nethackinformation/lutz/Item.java
+++ src/annahack/nethackinformation/lutz/Item.java
@@ -1,6 +1,11 @@
 package annahack.nethackinformation.lutz;
 
+import annahack.telnetconnection.TerminalSymbol;
+
 public interface Item
 {
+	public TerminalSymbol mapCharacter();
+	
+	public ItemClass itemClass();
 	
 }
