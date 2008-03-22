40c21c845e15a0068b6851e7fb0c1cd4bfde12d5
mattwelborn <mattwelborn@mail.utexas.edu>
2008-03-22 17:38:02 -0400

    Added skeletal PlayerRace and TheHero classes

---
+++ src/annahack/nethackinformation/nethackcreatures/TheHero.java
@@ -0,0 +1,55 @@
+package annahack.nethackinformation.nethackcreatures;
+
+import annahack.nethackinformation.nethackplayer.*;
+
+public class TheHero implements NetHackHero 
+{
+	private byte align;
+	private PlayerClass pClass;
+	private PlayerRace pRace;
+	private boolean isFem;
+	private boolean hasGender;
+	
+	public TheHero()
+	{
+		align=1;
+		hasGender=true;
+		isFem=true;
+		//pRace=PlayerRace.human
+		//pClass=PlayerClass.valkerie
+	}
+	
+	public TheHero(PlayerClass pclass, PlayerRace race, boolean isFemale, byte alignment)
+	{
+		pClass=pclass;
+		pRace=race;
+		isFem=isFemale;
+		hasGender=true;
+		align=alignment;
+	}
+	
+	public boolean isFemale()
+	{
+		return isFem;
+	}
+	
+	public boolean hasGender()
+	{
+		return hasGender;
+	}
+	
+	public PlayerClass getPlayerClass()
+	{
+		return pClass;
+	}
+	
+	public PlayerRace getPlayerRace()
+	{
+		return pRace;
+	}
+	
+	public byte alignment()
+	{
+		return align;
+	}
+}
