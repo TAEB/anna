91c111515c6e0070799786786e5763b608002762
mattwelborn <mattwelborn@mail.utexas.edu>
2008-04-03 00:20:38 -0400

    Added Dice class for handling multiple dice of same type (mainly as helper to DiceSet)

---
+++ src/annahack/utils/Dice.java
@@ -0,0 +1,30 @@
+package annahack.utils;
+
+public class Dice 
+{
+	private int num;
+	private int max;
+	public Dice(String d) throws BadDiceException
+	{
+		String[] a=d.split("d");
+		try
+		{
+			num=Integer.parseInt(a[0]);
+			max=Integer.parseInt(a[1]);
+		}
+		catch(Exception e)
+		{
+			throw new BadDiceException();
+		}
+	}
+	
+	public int getMax()
+	{
+		return max;
+	}
+	
+	public int getNum()
+	{
+		return num;
+	}
+}
