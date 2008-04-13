892aefd81e24aa831c168cc501e6311e7334041f
mattwelborn <mattwelborn@mail.utexas.edu>
2008-04-13 02:01:18 -0400

    Added debug class

---
+++ src/annahack/utils/Debug.java
@@ -0,0 +1,28 @@
+package annahack.utils;
+
+import java.io.*;
+import java.sql.Timestamp;
+
+public class Debug 
+{
+	private static PrintStream p;
+	public static void writeLine(String s)
+	{
+		if(p==null)
+		{
+			try{p = new PrintStream(new FileOutputStream("anna.log", true));}
+			catch(FileNotFoundException e)
+			{
+				System.err.println(e);
+			}
+		}
+		p.println (new Timestamp(System.currentTimeMillis())+": "+s);
+	}
+	public static void close()
+	{
+		if(p!=null)
+		{
+			p.close();
+		}
+	}
+}
