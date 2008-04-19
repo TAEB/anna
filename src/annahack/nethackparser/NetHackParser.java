20c16280a61691a12d3fd0188b2e666416652c3e
mattwelborn <mattwelborn@mail.utexas.edu>
2008-04-19 03:30:19 -0400

    Fixed parseStatusLine(), it is now 300% A-OK!

--- src/annahack/nethackparser/NetHackParser.java
+++ src/annahack/nethackparser/NetHackParser.java
@@ -106,24 +106,24 @@ public class NetHackParser
 	}
 	
 	
-	private static final Pattern r_hp = Pattern.compile("HP:([\\d+])\\(([\\d+])\\)");
-	private static final Pattern r_mp = Pattern.compile("Pw:([\\d+])\\(([\\d+])\\)");
-	private static final Pattern r_dlvl = Pattern.compile("Dlvl:([\\d+])");
-	private static final Pattern r_gold = Pattern.compile("\\$:([\\d+])");
-	private static final Pattern r_ac = Pattern.compile("AC:([\\d+])");
-	private static final Pattern r_turn = Pattern.compile("T:([\\d+])");
-	private static final Pattern r_xp = Pattern.compile("Xp:([\\d+])/([\\d+])");
+	private static final Pattern r_hp = Pattern.compile("HP:([\\d]+)\\(([\\d]+)\\)");
+	private static final Pattern r_mp = Pattern.compile("Pw:([\\d]+)\\(([\\d]+)\\)");
+	private static final Pattern r_dlvl = Pattern.compile("Dlvl:([\\d]+)");
+	private static final Pattern r_gold = Pattern.compile("\\$:([\\d]+)");
+	private static final Pattern r_ac = Pattern.compile("AC:([\\d]+)");
+	private static final Pattern r_turn = Pattern.compile("T:([\\d]+)");
+	private static final Pattern r_xp = Pattern.compile("Xp:([\\d]+)/([\\d]+)");
 	
-	private static final Pattern r_st = Pattern.compile("St:([\\d+])");
-	private static final Pattern r_dx = Pattern.compile("Dx:([\\d+])");
-	private static final Pattern r_co = Pattern.compile("Co:([\\d+])");
-	private static final Pattern r_in = Pattern.compile("In:([\\d+])");
-	private static final Pattern r_wi = Pattern.compile("Wi:([\\d+])");
-	private static final Pattern r_ch = Pattern.compile("Ch:([\\d+])");
+	private static final Pattern r_st = Pattern.compile("St:([\\d]+)");
+	private static final Pattern r_dx = Pattern.compile("Dx:([\\d]+)");
+	private static final Pattern r_co = Pattern.compile("Co:([\\d]+)");
+	private static final Pattern r_in = Pattern.compile("In:([\\d]+)");
+	private static final Pattern r_wi = Pattern.compile("Wi:([\\d]+)");
+	private static final Pattern r_ch = Pattern.compile("Ch:([\\d]+)");
 	
 	private static final Pattern r_align = Pattern.compile("(Lawful|Chaotic|Neutral)");
 	
-	private static final Pattern r_score = Pattern.compile("S:([\\d+])");
+	private static final Pattern r_score = Pattern.compile("S:([\\d]+)");
 	/**
 	 * Parses the status line, updates if all is found
 	 * @returns false if all of objects on status line are not found
@@ -196,27 +196,27 @@ public class NetHackParser
 		try
 		{
 			//Begin top line
-			pw.setSt(Integer.parseInt(m_st.group()));
-			pw.setCo(Integer.parseInt(m_co.group()));
-			pw.setDx(Integer.parseInt(m_dx.group()));
-			pw.setIn(Integer.parseInt(m_in.group()));
-			pw.setWi(Integer.parseInt(m_wi.group()));
-			pw.setCh(Integer.parseInt(m_ch.group()));
+			pw.setSt(Integer.parseInt(m_st.group(1)));
+			pw.setCo(Integer.parseInt(m_co.group(1)));
+			pw.setDx(Integer.parseInt(m_dx.group(1)));
+			pw.setIn(Integer.parseInt(m_in.group(1)));
+			pw.setWi(Integer.parseInt(m_wi.group(1)));
+			pw.setCh(Integer.parseInt(m_ch.group(1)));
 			
 			pw.setAlignment(m_align.group());
 			
-			score=Integer.parseInt(m_score.group());
+			score=Integer.parseInt(m_score.group(1));
 			//End top line
 			
 			//Begin bottom line
 			//TODO: Add gold to inventory
-			dlvl= Integer.parseInt(m_dlvl.group());
-			turn= Integer.parseInt(m_turn.group());
+			dlvl= Integer.parseInt(m_dlvl.group(1));
+			turn= Integer.parseInt(m_turn.group(1));
 			pw.setHp(Integer.parseInt(m_hp.group(1)));
 			pw.setMaxHp(Integer.parseInt(m_hp.group(2)));
 			pw.setMp(Integer.parseInt(m_mp.group(1)));
 			pw.setMaxMp(Integer.parseInt(m_mp.group(2)));
-			pw.setAC(Integer.parseInt(m_mp.group()));
+			pw.setAC(Integer.parseInt(m_mp.group(1)));
 			pw.setXp(Integer.parseInt(m_xp.group(1)));
 			pw.setLevel(Integer.parseInt(m_xp.group(2)));
 			//End bottom line
